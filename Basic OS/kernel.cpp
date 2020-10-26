0bb4f97c09abc0c245c254dfa744b9eff4b44ddb
Shiv2101 <64523022+Shiv2101@users.noreply.github.com>
2020-10-26 20:30:37 +0530

    First Commit

---
+++ Basic OS/kernel.cpp
@@ -0,0 +1,30 @@
+
+#include "types.h"
+
+void printf(char* str)
+{
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    for(int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+}
+
+
+
+extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
+{
+    printf("Hello Everyone Welcome to my first os");
+
+    while(1);
+}
